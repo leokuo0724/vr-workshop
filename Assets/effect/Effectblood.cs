5b85b1707ff66d5315aff27f63e3722dca3742dd
MrWuMung <mc02746842@gmail.com>
2019-04-26 14:13:42 +0800

    20190426

---
+++ Assets/effect/Effectblood.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Effectblood : MonoBehaviour {
+
+    private float tempTime;
+
+    // Use this for initialization
+    void Start () {
+        tempTime = 0;
+        //獲取材質屬性
+        this.GetComponent<Renderer>().material.color = new Color
+        (
+                this.GetComponent<Renderer>().material.color.r,
+                this.GetComponent<Renderer>().material.color.g,
+                this.GetComponent<Renderer>().material.color.b,
+                //以下透明度
+                this.GetComponent<Renderer>().material.color.a
+        );
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (tempTime < 1)
+        {
+            tempTime = tempTime + Time.deltaTime;
+        }
+        if (this.GetComponent<Renderer>().material.color.a <= 1)
+        {
+            this.GetComponent<Renderer>().material.color = new Color
+            (
+                this.GetComponent<Renderer>().material.color.r,
+                this.GetComponent<Renderer>().material.color.g,
+                this.GetComponent<Renderer>().material.color.b,
+
+
+            //逐漸消失秒數
+            gameObject.GetComponent<Renderer>().material.color.a - tempTime / 4 * Time.deltaTime
+            );
+        }
+    }
+}
---
+++ Assets/effect/EffectbloodUI.cs
@@ -0,0 +1,77 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class EffectbloodUI : MonoBehaviour {
+
+    public float playerHP;
+    public float playerHPMax;
+    
+    private Color c;
+
+    //百分比數值
+    public float HP50;
+    private float HP30;
+    private float HP10;
+
+
+    //物體閃爍
+    private float shake;
+
+    // Use this for initialization
+    void Start () {
+        //假設玩家血量 之後刪除
+        playerHP = 10;
+        playerHPMax = 100;
+
+        //計算血量百分比
+        HP50 = playerHPMax/2;
+        HP30 = (playerHPMax * 3) / 10;
+        HP10 = (playerHPMax * 9) / 10;
+
+        //獲取圖片屬性
+        c =this.GetComponent<Image>().color;
+        c = new Color(c.r, c.g, c.b, 0 / 255f);
+
+        //this.GetComponent<Renderer>().material.color = new Color
+        //(
+        //        this.GetComponent<Renderer>().material.color.r,
+        //        this.GetComponent<Renderer>().material.color.g,
+        //        this.GetComponent<Renderer>().material.color.b,
+        //        //以下透明度
+        //        this.GetComponent<Renderer>().material.color.a
+        //);
+    }
+	
+	// Update is called once per frame
+	void Update () {
+
+        shake += Time.deltaTime;
+
+        if (playerHP<HP50)
+        {
+            if (shake % 1 > 0.5f)
+            {
+                c = new Color(c.r, c.g, c.b, 30 / 255f);
+                //this.GetComponent<SkinnedMeshRenderer>().material.color = new Color(
+                //this.GetComponent<SkinnedMeshRenderer>().material.color.r,
+                //this.GetComponent<SkinnedMeshRenderer>().material.color.g,
+                //this.GetComponent<SkinnedMeshRenderer>().material.color.b,
+                //this.GetComponent<SkinnedMeshRenderer>().material.color.a + 30);
+            }
+            else
+            {
+                c = new Color(c.r, c.g, c.b, 0 / 255f);
+                //this.GetComponent<SkinnedMeshRenderer>().material.color = new Color(
+                //this.GetComponent<SkinnedMeshRenderer>().material.color.r,
+                //this.GetComponent<SkinnedMeshRenderer>().material.color.g,
+                //this.GetComponent<SkinnedMeshRenderer>().material.color.b,
+                //this.GetComponent<SkinnedMeshRenderer>().material.color.a - 30);
+            }
+
+        }
+
+
+    }
+}
